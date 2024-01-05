421a5c5d445428ec9c5aa4d659eaf30c4bcc94fc
Max Thomas <maxthomas.vs@gmail.com>
2024-01-05 14:38:20 +0000

    Wraps cleaning of user table into function.

--- code/data_cleaning.py
+++ code/data_cleaning.py
@@ -21,58 +21,60 @@ class DataCleaning():
         df.dropna(axis=0, subset=columns, inplace=True)
 
 
+def clean_user_data():
 
-            
-   
+    #%%
+    users = DataExtractor().read_table('legacy_users')
+    cleaner = DataCleaning()
+
+    # %% [markdown]
+    # First, flag NULL as missing values. Then, convert to NaT.
+    #%%
+    print(users.info())
+    for column in users.columns:
+        users[column].loc[users[column] == 'NULL'] = pd.NaT   
+    print(users.info())
+
+    msno.matrix(users)
+
+    # %% [markdown]
+    # We see that several rows (21 total) are entirely missing, so we drop these.
+
+    #%%
+    cleaner.drop_null_rows(users, ['first_name'])
+
+    print(users.info())
+
+    # %% [markdown] Next we convert dates to datetime objects.
+    # There are 25 dob and 23 join dates that cannot be converted. On inspection, these are clearly not dates and we drop these rows too.
+    # %%
+    dates = ['date_of_birth','join_date']
+    cleaner.make_datetime(users, dates)
+
+    print(users[['date_of_birth','join_date']].info())
+
+    cleaner.drop_null_rows(users, ['date_of_birth','join_date'])    
+
+    print(users.info())
+
+    # %% [markdown] We also make a small change to correct country codes.
+    users['country_code'].loc[users['country_code'] == 'GGB'] = 'GB'
+
+    # %% [markdown] Finally, we check that all remaining rows have a user id of 36 length and are all lowercase
+    correct_uid = (users['user_uuid'].str.len() == 36) & (users['user_uuid'].str.islower())
+    print(users['user_uuid'].loc[~correct_uid.values])
+
+    # %% [markdown] Categorical are converted for categorical columns.
+    categoricals = ['country','country_code']
+    cleaner.make_categorical(users, categoricals)
+
+    # %% [markdown] We are left with 15266 rows
+    users.info()
+
+    return users
+
+if __name__ == '__main__':
+    clean_user_data()
 
 
-#if __name__ == '__main__':
-#%%
-users = DataExtractor().read_table('legacy_users')
-cleaner = DataCleaning()
 
-# %% [markdown]
-# First, flag NULL as missing values. Then, convert to NaT.
-#%%
-print(users.info())
-for column in users.columns:
-    users[column].loc[users[column] == 'NULL'] = pd.NaT   
-print(users.info())
-
-msno.matrix(users)
-
-# %% [markdown]
-# We see that several rows (21 total) are entirely missing, so we drop these.
-
-#%%
-cleaner.drop_null_rows(users, ['first_name'])
-
-print(users.info())
-
-# %% [markdown] Next we convert dates to datetime objects.
-# There are 25 dob and 23 join dates that cannot be converted. On inspection, these are clearly not dates and we drop these rows too.
-# %%
-dates = ['date_of_birth','join_date']
-cleaner.make_datetime(users, dates)
-
-print(users[['date_of_birth','join_date']].info())
-
-cleaner.drop_null_rows(users, ['date_of_birth','join_date'])    
-
-print(users.info())
-
-# %% [markdown] We also make a small change to correct country codes.
-users['country_code'].loc[users['country_code'] == 'GGB'] = 'GB'
-
-# %% [markdown] Finally, we check that all remaining rows have a user id of 36 length and are all lowercase
-correct_uid = (users['user_uuid'].str.len() == 36) & (users['user_uuid'].str.islower())
-print(users['user_uuid'].loc[~correct_uid.values])
-
-# %% [markdown] Categorical are converted for categorical columns.
-categoricals = ['country','country_code']
-cleaner.make_categorical(users, categoricals)
-
-# %% [markdown] We are left with 15266 rows
-users.info()
-
-# %%
