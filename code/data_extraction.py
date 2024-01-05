cafc4b58a28a048ad1d6a94367e6a76f7065b929
Max Thomas <maxthomas.vs@gmail.com>
2024-01-05 14:56:00 +0000

    Works with updated db_utils.

--- code/data_extraction.py
+++ code/data_extraction.py
@@ -1,42 +1,24 @@
-import yaml
-import sqlalchemy as sqla
-from sqlalchemy import inspect
+import pandas as pd
+import db_utils as dbu
 
 class DataExtractor():
     
-    def __init__(self, filename='../setup/db_credentials.yaml'):
-        self.credentials = self.read_credentials(filename)
-        self.engine = self.start_sqla_engine()
-        self.tables = self.list_db_tables()
-        
-    
-    def read_credentials(self, filename):
-        with open(filename, 'r') as fn:
-            yaml_as_dict = yaml.safe_load(fn)
-        return yaml_as_dict
-    
-    def start_sqla_engine(self):
-        '''
-        Creates engine to connect to RDS.
-        '''
-        url = sqla.engine.url.URL.create(**self.credentials) # converts credentials into url
-        return sqla.create_engine(url)
-    
-    def list_db_tables(self):
-        '''
-        Lists all tables in the database.
-        '''
-        inspector = inspect(self.engine)
-        return inspector.get_table_names()
-    
-    def print_table_names(self):
+    def print_table_names(self, engine):
         '''
         Prints all tables in the database.
         '''
-        print(self.tables)
+        print(engine.tables)
+
+    def read_rds_table(self, engine, table_name):
+        '''
+        Reads a table from the database.
+        '''
+        return pd.read_sql_table(table_name, engine)
     
 
 
 if __name__ == '__main__':
-    data_extractor = DataExtractor()
-    data_extractor.print_table_names()
+    connection = dbu.DatabaseConnector()
+    extractor = DataExtractor()
+    users = extractor.read_rds_table(connection.engine, 'legacy_users')
+    print(users.info())
