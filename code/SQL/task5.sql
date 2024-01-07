4d2ac8a2ae76afd56070b10b8d33d4ce77399bc1
Max Thomas <maxthomas.vs@gmail.com>
2024-01-07 15:46:51 +0000

    Completes more queries.

--- code/SQL/task5.sql
+++ code/SQL/task5.sql
@@ -1,23 +1,15 @@
-SELECT COUNT(orders_table.date_uuid) AS number_of_sales,
-	   SUM(orders_table.product_quantity * dim_products*price_gbp) AS total_sales,
-	   CASE 
-	        WHEN dim_store_details.store_type = 'Web Portal' THEN 'Web'
-		    ELSE 'Offline' 
-	   END AS location
+SELECT store_type,
+	   SUM(orders_table.product_quantity * dim_products.price_gbp) AS total_sales,
+	   SUM(orders_table.product_quantity * dim_products.price_gbp) / 
+	   (SELECT SUM(orders_table.product_quantity * dim_products.price_gbp)
+		FROM orders_table
+		JOIN
+			dim_products ON orders_table.product_code = dim_products.product_code) AS pct_total
 FROM orders_table
 JOIN
-	dim_store_details ON dim_store_details.store_code = orders_table.store_code
-GROUP BY location
-ORDER BY location DESC
-
-
-+-------------+-------------+---------------------+
-| store_type  | total_sales | percentage_total(%) |
-+-------------+-------------+---------------------+
-| Local       |  3440896.52 |               44.87 |
-| Web portal  |  1726547.05 |               22.44 |
-| Super Store |  1224293.65 |               15.63 |
-| Mall Kiosk  |   698791.61 |                8.96 |
-| Outlet      |   631804.81 |                8.10 |
-+-------------+-------------+---------------------+
-
+	dim_store_details ON dim_store_details.store_code = orders_table.store_code	  
+JOIN 
+	dim_products ON orders_table.product_code = dim_products.product_code
+GROUP BY store_type
+ORDER BY total_sales DESC
+	
